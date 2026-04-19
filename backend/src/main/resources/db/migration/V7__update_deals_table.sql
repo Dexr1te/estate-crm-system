836d3dd36423b5c293187b63eae0041ad6ba1687
Sultan <asansultan25@gmail.com>
2026-04-19 13:58:54 +0500

    remove backend from repo

--- backend/src/main/resources/db/migration/V7__update_deals_table.sql
+++
@@ -1,7 +0,0 @@
--- V7__update_deals_table.sql
-
--- 1. Добавить бюджет клиента в сделку
-ALTER TABLE deals ADD COLUMN budget NUMERIC(15,2);
-
--- 2. Сделать property_id необязательным (для этапа LEAD)
-ALTER TABLE deals ALTER COLUMN property_id DROP NOT NULL;
