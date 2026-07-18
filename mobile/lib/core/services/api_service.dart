bf6e6b0d5ab70aad726633373b49d7dcfb49da0f
Sultan <asansultan25@gmail.com>
2026-07-18 16:04:03 +0500

    changed api url

--- mobile/lib/core/services/api_service.dart
+++ mobile/lib/core/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:real_estate_crm/core/models/models.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-const _baseUrl = 'http://localhost:8080/api';
+const _baseUrl = 'https://estate-crm-system.onrender.com/api';
 
 class ApiService {
   static final ApiService _instance = ApiService._internal();
